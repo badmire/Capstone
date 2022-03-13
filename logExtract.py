4447ce355fca453b5483f5095296159c694a6abe
brandon-admire-osu <77756635+brandon-admire-osu@users.noreply.github.com>
2022-03-13 15:33:45 -0700

    Update logextract for final diff format

--- logExtract.py
+++ logExtract.py
@@ -1,50 +1,76 @@
+from cgi import test
 import csv
 import sys
 
-commit_hash = ""
-bump_version = ""
+total_changes_diff = 0
+total_insertions_diff = 0
+total_deletions_diff = 0
+total_touched_diff = 0
+git_div_ver_diff = str()
 output = []
 
 if len(sys.argv) < 2:
     print("Must have arguments, pass -h for help")
     exit()
 elif sys.argv[1] == "-h":
-    print("1st argument == source, 2nd argument == destination")
+    print("1st argument == source")
     exit()
 
+out_file = sys.argv[1].split("_diff")[0] + ".csv"
+allLines = []
+
 with open(sys.argv[1], "r") as target:
+    allLines = []
     for line in target.readlines():
+        allLines.append(line)
 
-        # Check for meta data and assign to globals for future use
-        if line.split()[0] == "Bump":
-            bump_version = line.split()[-1]
-            continue
-        elif line.split()[0] == "Commit":
-            commit_hash = line.split()[1]
-            continue
-        elif line.split()[0] == "Changes" or line.split()[0] == "Summary":
-            continue
+git_div_ver = allLines[0].split()[2]  # Git diff ver
+total_changes_dif = allLines[-1].split()[0]  # Total changes for entire diff
+total_insertions_dif = allLines[-1].split()[3]  # Total insertions for entire diff
+total_deletions_dif = allLines[-1].split()[5]  # total deletions for entire diff
 
-        # Handle all regular rows assuming the format: blah ... blah *filepath* (diff)
-        if line.split()[-1] != "(diff)":
-            print("Unexpected format, exiting")
-            print(f"problem with: {line}")
-            exit(-1)
-        else:
-            current = line.split()[-2]
-            ftype = ""
+del allLines[0]
+del allLines[-1]
 
-            if "." not in current.split("/")[-1]:
-                ftype = "generic"
-            else:
-                ftype = current.split("/")[-1].split(".")[-1]
+total_touched_dif = len(allLines)  # Total number of files changed on diff
 
-            # Append to list for later writing to csv
-            output.append([current, ftype, commit_hash, bump_version])
+for line in allLines:
+    current = []
+    current_target = line.split()
+    current.append(current_target[0])  # File name
+    try:
+        current.append(current_target[0].split(".")[1])  # File extension
+    except:
+        current.append("none")
 
-with open(sys.argv[2], "w+", newline="") as out_target:
+    current.append(line.split("|")[1].count("+"))  # Total additions for file
+    current.append(line.split("|")[1].count("-"))  # Total deletions for file
+    current.append(current[-2] + current[-1])  # Total changes for file
+
+    current.append(git_div_ver)
+    current.append(total_changes_dif)
+    current.append(total_insertions_dif)
+    current.append(total_deletions_dif)
+    current.append(total_touched_dif)
+
+    output.append(current)
+
+with open(out_file, "w", newline="") as out_target:
     csvwriter = csv.writer(out_target, delimiter=",", quotechar="|")
-    csvwriter.writerow(["Filename", "file extension", "commit hash", "bump version"])
+    csvwriter.writerow(
+        [
+            "Filename",
+            "file extension",
+            "total changes for file",
+            "total additions for file",
+            "total deletions for file",
+            "diff ver",
+            "total changes for diff",
+            "total addtions for diff",
+            "total deletions for diff",
+            "total number of files changed for diff",
+        ]
+    )
 
     for row in output:
         csvwriter.writerow(row)
