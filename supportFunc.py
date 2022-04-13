107c43a158e80c57cdb2e6fdd976e51394414ac7
brandon-admire-osu <admireb@oregonstate.edu>
2022-04-13 13:29:18 -0700

    added version match function

--- supportFunc.py
+++ supportFunc.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 
 def confidenceThreshold(prediction_list):
@@ -24,21 +25,47 @@ def versionMatch(diffs, tests):
     Each entry in the output dictionary is a key value pair, with the key the version type, and the value a tuple. The first item in the tuple will be the appropriate diff csv that matches the version. The second item in the tuple will be an array of all test result csv that match the version.
 
     """
-    pass
-    # your code here
+    output = {}
+    for diff in diffs:
+        # Each dictionary value is an array, [*path to diff*, [*array of paths to tests*]]
+        output[re.findall("\d+_\d+_\d+_\d+", diff)[0]] = [diff, []]
+
+    for test in tests:
+        try:
+            output[re.findall("\d+_\d+_\d+_\d+", test)[0]][1].append(test)
+        except:  # If no match is found
+            print(
+                f"{test} does not appear to be a file with correctly formatted version number in it's path."
+            )
+            continue
+
+    return output
 
 
 if __name__ == "__main__":
+    from tkinter.filedialog import askopenfilenames
 
-    # Informal test for confidenceThreshold
-    test_list = []
-    for i in range(50):
-        test_list.append(
-            {
-                "prediction": "SUCCESS" if random.randint(0, 1) else "FAILURE",
-                "confidence": random.random(),
-            }
-        )
-    result = confidenceThreshold(test_list)
-    for thing in result:
-        print(thing)
+    # Informal test for versionMatch
+    diffs_paths = askopenfilenames(title="Select file", filetypes=[("Data", ("*.csv"))])
+    test_paths = askopenfilenames(title="Select file", filetypes=[("Data", ("*.csv"))])
+
+    result = versionMatch(diffs_paths, test_paths)
+
+    for k, v in result.items():
+        print(k)
+        print(f"diff: {v[0]}")
+        for path in v[1]:
+            print(f"    {path}")
+
+    # # Informal test for confidenceThreshold
+    # test_list = []
+    # for i in range(50):
+    #     test_list.append(
+    #         {
+    #             "prediction": "SUCCESS" if random.randint(0, 1) else "FAILURE",
+    #             "confidence": random.random(),
+    #         }
+    #     )
+    # result = confidenceThreshold(test_list)
+    # for thing in result:
+    #     print(thing)
