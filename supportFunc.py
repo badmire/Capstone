2920415ff05d06b31a7d77c08c663009e024fbab
brandon-admire-osu <admireb@oregonstate.edu>
2022-04-14 19:01:57 -0700

    Rearranged function placement, reworked versionMatch

--- supportFunc.py
+++ supportFunc.py
@@ -1,14 +1,15 @@
 import random
 import re
+from tkinter.filedialog import askopenfilenames
 
 
 def confidenceThreshold(prediction_list):
     """
-    Take in a list of dictionaries stored as prediction_list and order it according to the following structure: 
+    Take in a list of dictionaries stored as prediction_list and order it according to the following structure:
         1. All failed tests
         2. Confidence: low to high
 
-    Each dictionary in the list has a key for “prediction” which is either the string “SUCCESS” or “FAILURE”. Each dictionary has a key for “confidence” which is a number 0-1. 1 being 100% confident and 0 being 0% confident. 
+    Each dictionary in the list has a key for “prediction” which is either the string “SUCCESS” or “FAILURE”. Each dictionary has a key for “confidence” which is a number 0-1. 1 being 100% confident and 0 being 0% confident.
 
     Sort the list to have all failed tests at the start of the list and then have the lowest confident tests next. The end of the list therefore has the highest confidence test prediction.
 
@@ -18,13 +19,22 @@ def confidenceThreshold(prediction_list):
     return sorted(prediction_list, key=lambda x: (x["prediction"], x["confidence"]))
 
 
-def versionMatch(diffs, tests):
-    """
-    Takes in an array of diff csv and an array of test csv, matches by version type, and returns dictionary object with matched key pairs.
+def loadFiles(File_Type):
+    # THIS FUNCTION NEEDS TO CHANGE TO ACCEPT A PATH W/O HUMAN INTERACTION
+    # A command line argument perhaps?
+    file_names = askopenfilenames(title="File_Type", filetypes=[("Data", ("*.csv"))])
+    # print(file_names)
+    return file_names
 
-    Each entry in the output dictionary is a key value pair, with the key the version type, and the value a tuple. The first item in the tuple will be the appropriate diff csv that matches the version. The second item in the tuple will be an array of all test result csv that match the version.
+
+def versionMatch(diff_dir, test_dir):
+    """
+    Take in path to a directory for diffs, and one for tests, return dictionary with diffs matched to test sets.
 
     """
+    diffs = loadFiles("Diff csvs")
+    tests = loadFiles("Test csvs")
+
     output = dict()
     for diff in diffs:
         # Each dictionary value is an array, [*path to diff*, [*array of paths to tests*]]
