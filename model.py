a380fe5221d050a9bd93ad3a378ffa810edd6b9a
Brandon Admire <admireb@oregonstate.edu>
2022-05-17 21:01:51 -0700

    refactoring, WIP

--- model.py
+++ model.py
@@ -3,12 +3,15 @@
 # 2. Setup data into test and training set
 # 3. Create Model based on data
 # 4. Add additional CSV's and associate correctly
+
 from pycaret.classification import *
-import pandas as pd
 import sys
 
+from model_funcs import *
 from supportFunc import *
 from datetime import datetime
+
+
 
 
 if (len(sys.argv) != 2):
@@ -53,106 +56,23 @@ if newModel == False:
     os.chdir("..")
 
 if newModel == True:
-    # Load and match diffs to tests
-    result = versionMatch()
+    createNewModel("./diffs","./tests")
 
-    # Load tests and condense them into TestStruct class
-    tests = readTests(result)
-
-    # Load diffs/features
-    diffs = loadDiffs(result)
-
-    # Currently possible tags:
-    # From diffs:
-    # "total_change", "total_add", "total_del", "total_fchange",
-    # From tests:
-    # "child_link","parent_test_chain","child_result","parent_link","parent_start_date","sw_version","result","run_time","error_message","instrument_name","instrument_git_hash","run_date","collection_date","dut_console_log","is_system_test","connection_type","visa_name","test_git_hash","ptf_git_hash","test_log_file","test_name","test_requirements","test_description","scenario_number","expected_skipped_models","linked_issues_snapshot","seed"
-    # Misc:
-    # "historic"
-
-    # Special:
-    # "fchange"
-
-    numerical_tags = ["total_change", "total_add", "total_del", "total_fchange"]
-
-    categorical_tags = []
-
-    # For tags that produce more columns or have special logic
-    special_tags = ["fchange"]
-
-    final_set = tableCreate(
-        numerical_tags + categorical_tags + special_tags, tests, diffs)
-
-    # Adjust which columns to include here
-    if "fchange" in special_tags:
-        file_names = fileChange(diffs)
-        for file in file_names:
-            numerical_tags.append(f"{file}_change")
-            numerical_tags.append(f"{file}_del")
-            numerical_tags.append(f"{file}_add")
-            categorical_tags.append(f"{file}_name")
-            categorical_tags.append(f"{file}_extension")
-
-    print("*************************************")
-    print("***Processing done, starting model***")
-    print("*************************************")
-    
-
-
-    dataset = pd.DataFrame(final_set)
-
-    target_data = dataset.sample(frac=0.95, random_state=786)
-    data_unseen = dataset.drop(target_data.index)
-    target_data.reset_index(inplace=True, drop=True)
-    data_unseen.reset_index(inplace=True, drop=True)
-    print("Data for Modeling: " + str(target_data.shape))
-    print("Unseen Data For Predictions: " + str(data_unseen.shape))
-
-    s = setup(
-        target_data,
-        target="result",
-        numeric_features=numerical_tags,
-        categorical_features=categorical_tags,
-        silent=True,
-        remove_perfect_collinearity=False
-    )
-
-    # Create Logitic regression model
-
-    # best = compare_models()
-    model = create_model("lr")
-
-    # Else we have already loaded a model into the lr variable
-
-    # Else we have already loaded a model into the lr variable
-
-    # ****************************
-    # Tune and then save the model "lr" here:
-    # ****************************
-
-    print("Tuning the new model...")
-    # lr = ensemble_model(best, method='Boosting', choose_better=True)
-    model = tune_model(model)
-    print("Sucessfully tuned model.")
-    plot_model(model, save=True)
-    os.chdir(os.getcwd() + "/models")
-    print("Please enter the name of the model to be saved: ")
-    model_name = input()
-    save_model(model, model_name)
-    os.chdir("..")
 
 if (newModel == False):
-    # Make target_data only use the new diff we are trying to analyse
-    pass
+    # function currently a placeholder
+    forcastPredictions("path/to/diff",model)
+
+
 
 # Predict
-predictions = predict_model(model, data=target_data)
-os.chdir(os.getcwd()+"/predictions")
-now = datetime.now()
-dateString = str(now)
-dateString = dateString[:16]
-dateString = dateString.replace(':', '-')
-predictionName = dateString+".csv"
-print(predictionName)
-predictions.to_csv(dateString+".csv")
-os.chdir("..")
+# predictions = predict_model(model, data=target_data)
+# os.chdir(os.getcwd()+"/predictions")
+# now = datetime.now()
+# dateString = str(now)
+# dateString = dateString[:16]
+# dateString = dateString.replace(':', '-')
+# predictionName = dateString+".csv"
+# print(predictionName)
+# predictions.to_csv(dateString+".csv")
+# os.chdir("..")
